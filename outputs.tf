32d4091a2da74dc300915ea85f0df758ef6b1d14
Isaac Ma <isaac_ma@live.com>
2020-09-17 01:42:22 -0400

    Adding proper references to output from namespace resource

--- outputs.tf
+++ outputs.tf
@@ -3,17 +3,17 @@ output "name" {
 }
 
 output "generation" {
-  value = kubernetes_namespace.main.generation
+  value = kubernetes_namespace.main.metadata.0.generation
 }
 
 output "resource_version" {
-  value = kubernetes_namespace.main.resource_version
+  value = kubernetes_namespace.main.metadata.0.resource_version
 }
 
 output "url" {
-  value = kubernetes_namespace.main.self_link
+  value = kubernetes_namespace.main.metadata.0.self_link
 }
 
 output "uid" {
-  value = kubernetes_namespace.main.uid
+  value = kubernetes_namespace.main.metadata.0.uid
 }
